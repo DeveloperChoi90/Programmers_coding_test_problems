fe314e83251f997c5891423e53fa662c41364db5
DeveloperChoi90 <DeveloperChoi90@gmail.com>
2022-11-15 01:57:06 +0900

    programmers coding test problem level 1: 신규아이디 추천(정규식표현)

---
+++ Programmers_newID.java
@@ -0,0 +1,41 @@
+import java.util.Locale;
+
+public class Programmers_newID {
+    public static void main(String[] args) {
+        String new_id = "...!@BaT#*..y.abcdefghijklm";
+        Solution sol = new Solution();
+        System.out.println(sol.solution(new_id));
+    }
+
+    public static class Solution {
+        public String solution(String new_id) {
+            StringBuilder answer = new StringBuilder(new_id);
+            // 1단계
+            answer = new StringBuilder(answer.toString().toLowerCase());
+            // 2단계
+            answer = new StringBuilder(answer.toString().replaceAll("[^-_.a-z0-9]", ""));
+            // 3단계
+            answer = new StringBuilder(answer.toString().replaceAll("[.]{2,}", "."));
+            // 4단계
+            answer = new StringBuilder(answer.toString().replaceAll("^[.]|[.]$", ""));
+            // 5단계
+            if(answer.toString().equals("")) {
+                answer.append("a");
+            }
+            // 6단계
+            if(answer.length() >= 16) {
+                answer = new StringBuilder(answer.substring(0, 15));
+                if(answer.charAt(14) == '.') {
+                    answer = new StringBuilder(answer.toString().replaceAll("[.]$", ""));
+                }
+            }
+            // 7단계
+            if(answer.length() <= 2) {
+                while (answer.length() < 3) {
+                    answer.append(answer.charAt(answer.length() - 1));
+                }
+            }
+            return answer.toString();
+        }
+    }
+}
